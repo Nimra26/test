56f075d5fac2824ba1e3787b71346dbf9e5c02c2
Nimra26 <nimra26399@gmail.com>
2022-10-24 12:04:46 +0500

    fisrt meeting

---
+++ swap.py
@@ -0,0 +1,10 @@
+A = int(input("Enter the 1st number to swap: "))
+B = int(input("Enter the 2nd number to swap: "))
+
+A=A+B
+B=A-B
+A=A-B
+
+print("----After swapping----")
+print("1st number: ", A)
+print("2nd number: ",B)
